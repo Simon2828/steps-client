4d95e21588df0ac587d9e62a65b6f52bc9289e28
simon2828 <simon.tanner28@gmail.com>
2018-08-05 17:57:03 +0100

    update api

--- src/api.js
+++ src/api.js
@@ -1,27 +1,27 @@
 import superagent from 'superagent'
-import { API_HOST } from './config'
+import config from './config'
 
 class Api {
     
     getAllLos = () => (
       superagent
-      .get(`${API_HOST}/steps`)
+      .get(`${config}/steps`)
     )
  
-    editLo = (originalLO, loValue, stepIndex, databaseId)  => {
-      return superagent
-      .post(`${API_HOST}/steps/edit-lo`)
-      .send({originalLO, lO: loValue, id: stepIndex.index, databaseId})
-      .set('Accept', 'application/json')
+    // editLo = (originalLO, loValue, stepIndex, databaseId)  => {
+    //   return superagent
+    //   .post(`${API_HOST}/steps/edit-lo`)
+    //   .send({originalLO, lO: loValue, id: stepIndex.index, databaseId})
+    //   .set('Accept', 'application/json')
       
-    }
+    // }
     
-    editStep = (originalStep, stepValue, stepIndex, databaseId) => {
-      return superagent
-      .post(`${API_HOST}/steps/edit-step`)
-      .send({originalStep, step: stepValue, stepIndex, databaseId})
-      .set('Accept', 'application/json')
-    }
+    // editStep = (originalStep, stepValue, stepIndex, databaseId) => {
+    //   return superagent
+    //   .post(`${API_HOST}/steps/edit-step`)
+    //   .send({originalStep, step: stepValue, stepIndex, databaseId})
+    //   .set('Accept', 'application/json')
+    // }
 
     // need the target value to create new entry into database using primary key..
 
